6695833e92a4544e48acaaf1eb73a7c3b4fb0270
delonhenrique <delon.henrique@gmail.com>
2018-10-04 15:46:02 -0300

    Updates

---
+++ precache-manifest.ea1f54faf3ea437f8f75e5523fd77fa4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "7251814a21aaab420775",
+    "url": "/react-tutorial/static/js/main.7251814a.chunk.js"
+  },
+  {
+    "revision": "855276873adeae1ea0f1",
+    "url": "/react-tutorial/static/js/1.85527687.chunk.js"
+  },
+  {
+    "revision": "7251814a21aaab420775",
+    "url": "/react-tutorial/static/css/main.551043a8.chunk.css"
+  },
+  {
+    "revision": "fe9ac3b87512adf9897b3d5c810fb697",
+    "url": "/react-tutorial/index.html"
+  }
+];
